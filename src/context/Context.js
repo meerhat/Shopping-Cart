f375c7db7d48d1db7e5876b738149ee6ebabe5a4
Hera Tahreem <114937302+meerhat@users.noreply.github.com>
2023-07-05 16:53:56 +0530

    Update Context.js

--- src/context/Context.js
+++ src/context/Context.js
@@ -11,7 +11,7 @@ const Context = ({ children }) => {
     id: faker.datatype.uuid(),
     name: faker.commerce.productName(),
     price: faker.commerce.price(),
-    image: faker.random.image(),
+    image: `https://picsum.photos/200/200?random=${Math.random()}`,
     inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
     fastDelivery: faker.datatype.boolean(),
     ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
